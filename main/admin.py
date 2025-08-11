d2487c0fcbda061bfbf7cd1e3aa38e184a327cb3
yamaltdinov <yamaltdinov.r@gmail.com>
2025-08-11 23:53:40 +0500

    добавили модели: категории и продукт; зарегистрировали их в админке.

---
+++ main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'avaliable', 'created', 'updated']
+    list_filter = ['avaliable', 'created', 'updated', 'category']
+    list_editable = ['price', 'avaliable']
+    prepopulated_fields = {'slug':('name',)}
